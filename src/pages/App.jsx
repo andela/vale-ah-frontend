e307c97aed645f0fc00016c9343d3c704981febf
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2019-04-02 09:42:38 +0100

    chore(testing): setup testing with enzyme and jest

---
+++ src/pages/App.jsx
@@ -0,0 +1,10 @@
+import { hot } from 'react-hot-loader/root';
+import React from 'react';
+
+/**
+ * Render a dummy page to test react app
+ * @returns {JSX} Page Component
+ */
+const App = () => <h1>Welcome to Naija Chop Chop</h1>;
+
+export default hot(App);
