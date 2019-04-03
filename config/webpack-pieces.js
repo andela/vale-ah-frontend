ea4a1fe2a58a38ffde3d4309773c2dc35d006e11
devskope <pmskope@gmail.com>
2019-04-03 15:23:45 +0100

    chore(setup): Extend webpack config
    -minify css, js
    -autoprefix css
    
    [Finishes #164139721] [Finishes #164139727]

--- config/webpack-pieces.js
+++ config/webpack-pieces.js
@@ -1,12 +1,14 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OpitmizeCssPlugin = require('optimize-css-assets-webpack-plugin');
+const postCssPresetEnv = require('postcss-preset-env');
 
 exports.devServer = () => ({
   devServer: {
-    contentBase: './static',
+    contentBase: './build',
     historyApiFallback: true,
     hot: true,
     open: true,
-    overlay: true,
     stats: 'errors-only',
   },
 });
@@ -33,7 +35,7 @@ exports.loadJavascript = ({ include } = {}) => ({
     rules: [
       {
         include,
-        test: /\.(js|jsx)$/,
+        test: /\.jsx?$/,
         use: [
           'babel-loader',
           {
@@ -58,17 +60,60 @@ exports.loadStyles = ({ include } = {}) => ({
       },
       {
         test: /\.scss$/,
+        include,
         use: ['style-loader', 'css-loader', 'fast-sass-loader'],
       },
     ],
   },
 });
 
+exports.extractStyles = ({ include, use = [] } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', ...use],
+      },
+      {
+        test: /\.scss$/,
+        include,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'fast-sass-loader',
+          ...use,
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[chunkhash:8].css',
+    }),
+  ],
+});
+
+exports.transformCss = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: [postCssPresetEnv()],
+  },
+});
+
+exports.minifyStyles = ({ cssNanoConfig }) => ({
+  plugins: [
+    new OpitmizeCssPlugin({
+      cssProcessorPluginOptions: cssNanoConfig,
+    }),
+  ],
+});
+
 exports.loadImages = ({ include, options } = {}) => ({
   module: {
     rules: [
       {
-        test: /(.jpg|.jpeg|.svg|.png)$/,
+        test: /(.jpe?g|.svg|.png)$/,
         include,
         use: [
           {
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,13 +1,19 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
+const Dotenv = require('dotenv-webpack');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const {
+  autoPrefix,
   devServer,
+  extractStyles,
   loadHtml,
   loadImages,
   loadJavascript,
   loadStyles,
+  minifyStyles,
 } = require('./webpack-pieces');
 
 const baseConfig = merge([
@@ -20,35 +26,61 @@ const baseConfig = merge([
     output: {
       path: path.resolve(__dirname, '../build'),
       publicPath: '/',
-      filename: 'bundle.[hash].js',
+      filename: 'bundle.js',
     },
+    plugins: [new Dotenv()],
   },
-  loadStyles(),
   loadHtml(),
   loadJavascript({ include: path.join(__dirname, '../src') }),
 ]);
 
 const devConfig = merge([
   {
-    plugins: [new webpack.HotModuleReplacementPlugin()],
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new ErrorOverlayPlugin(),
+    ],
   },
   devServer(),
   loadImages(),
+  loadStyles(),
 ]);
 
 const prodConfig = merge([
   {
     output: {
-      path: path.resolve(__dirname, '../build'),
+      filename: 'bundle.[chunkhash:8].js',
       publicPath: './',
-      filename: 'bundle.[hash].js',
     },
-    devServer: {
-      historyApiFallback: true,
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          sourceMap: true,
+          terserOptions: {
+            output: {
+              comments: false,
+            },
+          },
+        }),
+      ],
     },
   },
+  extractStyles({ use: [autoPrefix()] }),
+  minifyStyles({
+    cssNanoConfig: {
+      preset: [
+        'default',
+        {
+          discardComments: { removeAll: true },
+          cssDeclarationSorter: { order: 'smacss' },
+        },
+      ],
+    },
+  }),
   loadImages({ options: { limit: 5000 } }),
 ]);
 
-module.exports = mode =>
-  merge(baseConfig, mode === 'development' ? devConfig : prodConfig, { mode });
+module.exports = env =>
+  merge(baseConfig, env === 'development' ? devConfig : prodConfig, {
+    mode: env,
+  });
