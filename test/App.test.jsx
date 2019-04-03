bd18ff67a8f53f8d74754188319b905b512f0e13
Uchenna A. Iheanacho <21208124+LordUche@users.noreply.github.com>
2019-04-03 09:54:44 +0100

    #164139723 Setup Testing with Enzyme and Jest (#2)
    * chore(testing): setup testing with enzyme and jest
    
    * style(editorconfig): add ending new line
    
    * chore(testing): setup continuous integration
    - configure travis-ci
    - configure codeclimate test coverage
    
    * ch(travis): add chrome addon to .travis.yml
    
    [Finishes #164139723]
    [Finishes #164139724]
    [Finishes #164139728]

---
+++ test/App.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+import App from '../src/App';
+
+configure({ adapter: new Adapter() });
+describe('<App />', () => {
+  it('h1 contains correct text', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('h1').text()).toBe('Welcome to Naija Chop Chop');
+  });
+  it('matches the snapshot', () => {
+    const tree = shallow(<App />);
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+});
